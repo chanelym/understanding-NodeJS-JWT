f69464d3d4c83041f72c2703aa65d61e9ab655e3
chanelym <chanelym@gmail.com>
2021-11-24 18:32:19 -0300

    Fixed Typo

--- src/controllers/adele.controllers.js
+++ src/controllers/adele.controllers.js
@@ -14,46 +14,47 @@ exports.registerNewAlbum = async (req, res) => {
   // Adele.validateInputCity(req, res);
 
   await Adele.create(req.body).then(() => {
-      res.status(201).json({ message: 'Album Successfully Created' });
+    res.status(201).json({ message: 'Album Successfully Created' });
   }).catch((err) => {
-      res.status(400).json({ message: 'Oops! Something went Wrong' });
-      console.error(err);
+    res.status(400).json({ message: 'Oops! Something went Wrong' });
+    console.error(err);
   });
 };
 
-exports.updateAlbum = async (req,res) => {
+exports.updateAlbum = async (req, res) => {
   // validations.validateURLID(req, res);
   // validations.validateInputCity(req, res);
 
-  await Adele.findByIdAndUpdate(req.params.id, req.body).then(() => { 
-      res.status(201).json({ message: 'Album Successfully Updated' });
+  await Adele.findByIdAndUpdate(req.params.id, req.body).then(() => {
+    res.status(201).json({ message: 'Album Successfully Updated' });
   }).catch((err) => {
-      console.error(err);
-      res.status(400).json( {message: 'Oops! Something went wrong'});
+    console.error(err);
+    res.status(400).json({ message: 'Oops! Something went wrong' });
   });
 };
 
-exports.deleteAlbum = async (req,res) => {
+exports.deleteAlbum = async (req, res) => {
   // Adele.validateURLID(req, res);
-  
-  await Adele.findByIdAndDelete( req.params.id ).then(() => { 
-      res.status(201).json({ message: 'Album Successfully Removed!' });
+  await Adele.findByIdAndDelete(req.params.id).then(() => {
+    res.status(201).json({ message: 'Album Successfully Removed!' });
   }).catch((err) => {
-      console.error(err);
-      res.status(400).json({ message: 'Oops! Something went wrong' });
+    console.error(err);
+    res.status(400).json({ message: 'Oops! Something went wrong' });
   });
+};
 
 exports.getAll = async (req, res) => {
   await Adele.find({}).then((adele) => {
-      res.status(200).json(adele);
-  })
+    res.status(200).json(adele);
+  });
 };
 
 exports.getAlbumByID = async (req, res) => {
   await Adele.findOne({ _id: req.params.id }).then((adele) => {
-      if (adele == null) { 
-          res.status(404).json({ message: 'Album Not Found' });
-      } else {
-          res.status(200).json(adele);
-      }})
+    if (adele == null) {
+      res.status(404).json({ message: 'Album Not Found' });
+    } else {
+      res.status(200).json(adele);
+    }
+  });
 };
